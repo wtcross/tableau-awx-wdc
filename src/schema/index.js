768ad31187f41328753c0178bf235fcc1cc1ecab
Tyler Cross <tyler@monad.attlocal.net>
2018-11-27 08:51:20 -0500

    Remove standard connections

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,27 +1,12 @@
 import jobs from './tables/jobs';
 import jobEvents from './tables/jobEvents';
 
-const connection = {
-  alias: 'joined job with job events',
-  tables: [
-    { id: 'jobEvents', alias: 'jobEvents' },
-    { id: 'jobs', alias: 'jobs' },
-  ],
-  joins: [
-    {
-      left: { tableAlias: 'jobs', columnId: 'id' },
-      right: { tableAlias: 'jobEvents', columnId: 'job' },
-      joinType: 'left',
-    },
-  ],
-};
-
 export default function build(eventDataQueries = []) {
   const schema = {
     tables: [
       jobs,
     ],
-    connections: [connection],
+    connections: [],
   };
 
   const eventDataColumns = eventDataQueries.reduce((accumulator, { name, role, type }) => {
